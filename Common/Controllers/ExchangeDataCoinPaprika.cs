6e1a307ca5b1f42a91ca396441b34164a6dec983
Brian Ogletree <brian.ogletree@gmail.com>
2020-06-14 14:48:47 -0600

    You'd think with a name like Coin'MarketCap' that they'd be able to calculate a market cap correctly. Switched to source from a research platform that can actually do math.

---
+++ Common/Controllers/ExchangeDataCoinPaprika.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using CoinpaprikaAPI.Entity;
+using CoinpaprikaAPI.Models;
+
+namespace Common.Controllers
+{
+    public class ExchangeDataCoinPaprika : IExchangeData
+    {
+        private static readonly string QuoteCurrency = ConfigurationManager.AppSettings["QuoteCurrency"];
+        private readonly CoinPaprikaEntity<List<TickerWithQuotesInfo>> _quoteData;
+        public string Id => "CoinPaprika";
+
+        public ExchangeDataCoinPaprika()
+        {
+            var client = new CoinpaprikaAPI.Client();
+            _quoteData = client.GetTickersAsync(new[] { QuoteCurrency }).Result;
+        }
+
+        public string GetName(string symbol)
+        {
+            var quotesInfo = _quoteData.Value.FirstOrDefault(x => x.Symbol == symbol.ToUpper());
+            return quotesInfo.Name;
+        }
+
+        public decimal GetPrice(string symbol)
+        {
+            var quotesInfo = _quoteData.Value.FirstOrDefault(x => x.Symbol == symbol.ToUpper());
+            return quotesInfo.Quotes[QuoteCurrency].Price;
+        }
+
+        public decimal GetPercentChange24Hour(string symbol)
+        {
+            var quotesInfo = _quoteData.Value.FirstOrDefault(x => x.Symbol == symbol.ToUpper());
+            return quotesInfo.Quotes[QuoteCurrency].PercentChange24H;
+        }
+    }
+}
